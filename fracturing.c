96dfc80235d1856cb6a0524229c91edd15455a57
Kamille-OConnor <ka835053@ucf.edu>
2024-09-05 13:22:33 -0400

    Create fracturing.c

---
+++ fracturing.c
@@ -0,0 +1,35 @@
+// Kamille O'Connor
+// 9.3.2024
+// COP3223 Section 0V06
+
+#include <stdio.h>
+#include <math.h>
+#define PI 3.14159
+
+// Distance Calculation
+double calculateDistance() {
+}
+
+// Perimeter Calculation
+double calculatePerimeter() {
+}
+
+// Area Calculation
+double calculateArea() {
+}
+
+// Width Calculation
+double calculateWidth() {
+}
+
+// Height Calculation
+double calculateHeight() {
+}
+
+// Extra Function
+double askForUserInput() {
+}
+
+int main(int argc, char **argv) {
+	return 1;
+}
